d06c79420875450845247e86f3dd991170fc4a61
utsho-2025 <utsho5939@gmail.com>
2025-05-02 11:30:45 -0600

    commit i forget to do on thursday

--- sketch.js
+++ sketch.js
@@ -1,38 +1,4 @@
-<<<<<<< Updated upstream
-// // module aliases
-// var Engine = Matter.Engine,
-//     Render = Matter.Render,
-//     Runner = Matter.Runner,
-//     Bodies = Matter.Bodies,
-//     Composite = Matter.Composite;
 
-// // create an engine
-// var engine = Engine.create();
-
-// // create a renderer
-// var render = Render.create({
-//     element: document.body,
-//     engine: engine
-// });
-
-// // create two boxes and a ground
-// var boxA = Bodies.rectangle(400, 200, 80, 80);
-// var boxB = Bodies.rectangle(450, 50, 80, 80);
-// var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-
-// // add all of the bodies to the world
-// Composite.add(engine.world, [boxA, boxB, ground]);
-
-// // run the renderer
-// Render.run(render);
-
-// // create runner
-// var runner = Runner.create();
-
-// // run the engine
-// Runner.run(runner, engine);
-
-=======
 
 // let boxX = 400;
 // // module aliases
@@ -67,20 +33,35 @@
 
 // // run the engine
 // Runner.run(runner, engine);
-let spr;
-function setup() {
-  createCanvas(400, 400);
-  spr = createSprite(
-    width/2, height/2, 40, 40);
-  spr.shapeColor = color(255);
-  spr.velocity.y = 0.5;
+// // let spr;
+// function setup() {
+//   createCanvas(400, 400);
+//   spr = createSprite(
+//     width/2, height/2, 40, 40);
+//   spr.shapeColor = color(255);
+//   spr.velocity.y = 0.5;
+// }
+// function draw() {
+//   background(50);
+//   drawSprites();
+// }
+// function mousePressed() {
+//   spr.position.x = mouseX;
+//   spr.position.y = mouseY;
+// }
+function setup(){
+  new Canvas("2:1");
+  let ball = new Sprite(); 
+  ball.diameter = 20;
+  ball.x = width/2-100;
+  ball.y = height/2;
+  ball.fill = "white";
+  ball.velocity.x = 2;
+  let block = new Sprite(width/2,height/2,20,20);
+
 }
-function draw() {
-  background(50);
-  drawSprites();
+
+function draw(){
+  background(220);
 }
-function mousePressed() {
-  spr.position.x = mouseX;
-  spr.position.y = mouseY;
-}
->>>>>>> Stashed changes
+
