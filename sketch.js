f02c8639e63e78acc751e7ac754547fc6754d57d
utsho-2025 <utsho5939@gmail.com>
2025-05-02 10:55:19 -0600

    commit i forget to do on thursday

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // // module aliases
 // var Engine = Matter.Engine,
 //     Render = Matter.Render,
@@ -31,3 +32,55 @@
 // // run the engine
 // Runner.run(runner, engine);
 
+=======
+
+// let boxX = 400;
+// // module aliases
+// let Engine = Matter.Engine,
+//   Render = Matter.Render,
+//   Runner = Matter.Runner,
+//   Bodies = Matter.Bodies,
+//   Composite = Matter.Composite;
+
+// // create an engine
+// let engine = Engine.create();
+
+// // create a renderer
+// let render = Render.create({
+//   element: document.body,
+//   engine: engine
+// });
+
+// // create two boxes and a ground
+// let boxA = Bodies.rectangle(boxX, 200, 80, 80);
+// let boxB = Bodies.rectangle(450, 50, 80, 80);
+// let ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+// // add all of the bodies to the world
+// Composite.add(engine.world, [boxA, boxB, ground]);
+
+// // run the renderer
+// Render.run(render);
+
+// // create runner
+// let runner = Runner.create();
+
+// // run the engine
+// Runner.run(runner, engine);
+let spr;
+function setup() {
+  createCanvas(400, 400);
+  spr = createSprite(
+    width/2, height/2, 40, 40);
+  spr.shapeColor = color(255);
+  spr.velocity.y = 0.5;
+}
+function draw() {
+  background(50);
+  drawSprites();
+}
+function mousePressed() {
+  spr.position.x = mouseX;
+  spr.position.y = mouseY;
+}
+>>>>>>> Stashed changes
