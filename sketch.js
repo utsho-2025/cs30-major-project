05a56384f5a58e7a793eb710b08a5af85ad9fecd
utsho-2025 <utsho5939@gmail.com>
2025-05-08 12:15:33 -0600

    deleted matter.js and failed attempt

--- sketch.js
+++ sketch.js
@@ -1,60 +1,15 @@
 
 
-// let boxX = 400;
-// // module aliases
-// let Engine = Matter.Engine,
-//   Render = Matter.Render,
-//   Runner = Matter.Runner,
-//   Bodies = Matter.Bodies,
-//   Composite = Matter.Composite;
 
-// // create an engine
-// let engine = Engine.create();
+let ball;
 
-// // create a renderer
-// let render = Render.create({
-//   element: document.body,
-//   engine: engine
-// });
-
-// // create two boxes and a ground
-// let boxA = Bodies.rectangle(boxX, 200, 80, 80);
-// let boxB = Bodies.rectangle(450, 50, 80, 80);
-// let ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-
-// // add all of the bodies to the world
-// Composite.add(engine.world, [boxA, boxB, ground]);
-
-// // run the renderer
-// Render.run(render);
-
-// // create runner
-// let runner = Runner.create();
-
-// // run the engine
-// Runner.run(runner, engine);
-// // let spr;
-// function setup() {
-//   createCanvas(400, 400);
-//   spr = createSprite(
-//     width/2, height/2, 40, 40);
-//   spr.shapeColor = color(255);
-//   spr.velocity.y = 0.5;
-// }
-// function draw() {
-//   background(50);
-//   drawSprites();
-// }
-// function mousePressed() {
-//   spr.position.x = mouseX;
-//   spr.position.y = mouseY;
-// }
 function setup(){
 
 
   new Canvas("2:1");
   let ball = new Sprite(); 
-  console.log(0;
+
+  console.log(0);
   ball.diameter = 20;
   ball.x = width/2-100;
   ball.y = height/2;
@@ -65,14 +20,19 @@ function setup(){
 
 
   }
+
+
+  console.log("working");
  
 }
 
 function draw(){
   background(220);
+
 }
 
 function mousePressed(){
   ball.moveTo(mouseX,mouseY, 1);
+
   console.log("working");
 }
