fd9f88337b8b267c21bd284aa35a4a2d83092d3c
utsho-2025 <utsho5939@gmail.com>
2025-05-19 15:32:04 -0600

    added pics for sprites

--- sketch.js
+++ sketch.js
@@ -7,39 +7,45 @@ let pipeImg;
 let marioImg;
 let ball;
 let wall;
+
+
 // let ball = new Sprite(); 
 function setup(){
+  world.gravity.y = 40;
+  new Canvas(windowWidth,windowHeight/2)
 
 
-  new Canvas("2:1");
-  ball = new Sprite(); 
-  // let box = new Sprite();
-  box.x = width/3+50;
-  console.log(0);
-  ball.diameter = 20;
-  ball.x = width/2-100;
-  ball.y = height/2;
-  ball.fill = "white";
-  ball.velocity.x = 2;
-  ball.gravity = -5;
+  // new Canvas("2:1");
+  // ball = new Sprite(); 
+  // // let box = new Sprite();
+  // box.x = width/3+50;
+  // console.log(0);
+  // ball.diameter = 20;
+  // ball.x = width/2-100;
+  // ball.y = height/2;
+  // ball.fill = "white";
+  // ball.velocity.x = 2;
+  // ball.gravity = -5;
 
-  for(let i = 0; i< 50; i++){
-    let block = new Sprite(width/2+random(-5,5),height/2+random(-5,5),20,20); 
+  // for(let i = 0; i< 50; i++){
+  //   let block = new Sprite(width/2+random(-5,5),height/2+random(-5,5),20,20); 
 
 
-  }
-  wall = new Sprite(width,height,20,width+900);
-  wall.fill = "green";
-  wall.rotation = 270;
+  // }
+  // wall = new Sprite(width,height,20,width+900);
+  // wall.fill = "green";
+  // wall.rotation = 270;
 
-  wall.collider = 'static';
-  console.log("working");
+  // wall.collider = 'static';
+  // console.log("working");
  
 }
 function preload(){
   marioImg = loadImage("Mario.png");
-  brickImg = loadImage("Brick.png");
+  groundImg = loadImage("Ground.png");
   questionBlockImg = loadImage("QuestionBlock.png");
+  pipeImg = loadImage("Pipe.png")
+  
   
 
 }
@@ -48,8 +54,8 @@ function draw(){
 
 }
 
-function mousePressed(){
-  ball.moveTo(mouseX,mouseY, 8);
+// function mousePressed(){
+//   ball.moveTo(mouseX,mouseY, 8);
 
-  console.log("working");
-}
+//   console.log("working");
+// }
